4ffc84111ef786544aae566d0f13dccc6af574b5
Kuldeep <51863998+Ksrgeeks@users.noreply.github.com>
2021-09-24 06:55:30 +0530

    Add files via upload

---
+++ Que.js
@@ -0,0 +1,28 @@
+// create our questions
+let questions = [
+    {
+        question : "What does HTML stand for? ",
+        imgSrc : "img/html.png",
+        choiceA : "Correct",
+        choiceB : "Wrong",
+        choiceC : "Wrong",
+        choiceD : "Wrong",
+        correct : "A"
+    },{
+        question : "What does CSS stand for?",
+        imgSrc : "img/css.png",
+        choiceA : "Wrong",
+        choiceB : "Wrong",
+        choiceC : "Wrong",
+        choiceD : "Correct",
+        correct : "D"
+    },{
+        question : "What does JS stand for?",
+        imgSrc : "img/js.png",
+        choiceA : "Wrong",
+        choiceB : "Wrong",
+        choiceC : "Correct",
+        choiceD : "Wrong",
+        correct : "C"
+    }
+];
